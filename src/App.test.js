b851e9a064937db6dfd4f01258b6980cc0691f73
Victor Parmar <vic@smalldata.tech>
2021-11-21 12:45:06 +0100

    initial cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders counter text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByText(/counter/i);
+  expect(counterElement).toBeInTheDocument();
 });
